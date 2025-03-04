b3d22ca601231a2dc07f5abdd1c69cccb2fcb067
Vijayalakshmigowda19 <125528718+Vijayalakshmigowda19@users.noreply.github.com>
2025-03-04 14:55:28 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main  {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner n = new Scanner(System.in);
+  System.out.println("enter a number");
+  x=n.nextInt();
+  System.out.println("enter a number");
+  y=n.nextInt();
+  sum=x+y;
+   System.out.println("sum is:"+sum);
+}
+}
